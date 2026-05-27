c2437177a98ccb38a618c06d07da4b8a5e94b7c9
Ewa <ewa@itdot.fr>
2026-05-27 17:04:26 +0200

    feat(back): base express + cors

---
+++ backend/src/server.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import 'dotenv/config';
+import cors from 'cors';
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+//n'autoriser que le front à faire des requêtes et credentials pour les cookies de session à venir.
+app.use(cors({
+    origin: 'http://localhost:3000',
+    credentials : true,
+}));
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Coucou');
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+})
