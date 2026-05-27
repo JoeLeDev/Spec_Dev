b0ba645b5468daa23eb8a1d93fede3467bc6a541
joeledev <jonathanluembe@yahoo.com>
2026-05-27 15:46:22 +0200

    chore: frontend setup

---
+++ frontend/src/main.js
@@ -0,0 +1,7 @@
+import './style.css'
+
+document.querySelector('#app').innerHTML = `
+  <main class="min-h-screen bg-slate-900 text-white flex items-center justify-center">
+    <h1 class="text-4xl font-bold">Tailwind fonctionne</h1>
+  </main>
+`
